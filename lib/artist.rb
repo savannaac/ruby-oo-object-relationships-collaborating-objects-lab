37ca9af56d7e2b1c539f150d3cbea6875e24ba5e
savanna chiang <savannaac@gmail.com>
2021-02-17 19:35:04 -0600

    Object Relationships Collaborating Objects/Song lab - 1 failure

---
+++ lib/artist.rb
@@ -0,0 +1,33 @@
+class Artist
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self 
+        @songs = []
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+
+    def add_song(song)
+        @songs << song
+        song.artist = self
+    end
+
+    def self.find_or_create_by_name(name)
+        artist = Artist.new(name)
+        artist.name = name
+        self.all.detect { |artist| artist.name == name }     
+    end
+
+    def print_songs
+        songs.each { |song| puts song.name }
+    end
+end
